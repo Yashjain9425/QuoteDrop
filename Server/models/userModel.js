d17df16bdd2c7ba8586567e880e6f8df1e50b14a
shivamkumar4344 <ydvshivam10@gmail.com>
2025-07-15 10:45:28 +0530

    added getUser,deleteUser,displayUsers,updateUsers,addUsers--> all admin routes

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema({
-    username:{type:String},
-    password:{type:String},
-    email:{type:String,unique:true},
+    username:{type:String,required:true},
+    password:{type:String,required:true},
+    email:{type:String,unique:true,required:true},
     role:{type:String,default:"user"},
     toSend:{type:Boolean,default:true}
 },
--- Server/routes/userRoute.js
+++ Server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { register,login, getUser } from "../controllers/userController.js";
+import { register,login, getUser, deleteUser, displayUsers, updateUsers, addUser } from "../controllers/userController.js";
 import { authenticate, authorize } from "../middlewares/auth.js";
 const router = express.Router();
 
@@ -9,7 +9,11 @@ router.post('/login',login);
 
 
 //admin routes
+router.get('/all-users',authenticate,authorize("admin"),displayUsers);
 router.get('/:id',authenticate,authorize("admin"), getUser);
+router.delete('/:id',authenticate,authorize("admin"),deleteUser);
+router.patch('/:id',authenticate,authorize("admin"),updateUsers);
+router.post("/add-user",authenticate,authorize("admin"),addUser);
 
 export default router;
 
